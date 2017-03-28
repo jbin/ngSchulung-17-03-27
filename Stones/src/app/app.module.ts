0ee07ea68f6e2e4298dedd6cc5b869db6216d7d6
Jens Binfet <kontakt@jensbinfet.de>
2017-03-28 09:49:47 +0200

    Pipes

--- Stones/src/app/app.module.ts
+++ Stones/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -16,7 +16,9 @@ import { ProductModule } from './product/product.module';
     HttpModule,
     ProductModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Stones/src/app/product/brutto.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'brutto'
+})
+export class BruttoPipe implements PipeTransform {
+
+  transform(value: number, tax?: number): any {
+    if (!tax) {
+      tax = 19;
+    }
+    return value * (1 + tax / 100);
+  }
+
+}
